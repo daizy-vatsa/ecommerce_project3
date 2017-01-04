475e7324ba4f36358c968acacb31a0a20a38a5c6
Tanu <tanuuu.21@gmail.com>
2017-01-04 15:09:58 +1100

    Connected to Mongoose database

---
+++ models/user.js
@@ -0,0 +1,59 @@
+//require libraries first
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+//declare mongoose Schema
+var Schema = mongoose.Schema;
+
+// The user schema attributes / characteristics / fields
+
+var UserSchema = new mongoose.Schema({
+  email: { type: String, unique: true, lowecase:true},
+  password: String,
+
+  profile: {
+    name: { type: String, default: ''},
+    picture: { type: String, default: ''}
+  },
+
+  address: String,
+  history: [{
+    date: Date,
+    paid: { type: Number, default: 0},
+    // item: { type: Schema.Types.ObjectId, ref: ''}
+
+  }]
+});
+
+// Hash the password before we even save it in the database
+//.pre save it before we actually save it
+
+UserSchema.pre('save', function(next) {
+  // make a varibale and this refer to usre schema
+  var user = this;
+  if (!user.isModified('password')) return next();
+  //if user not modifies
+  //salt is just the random data created with saltgen
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+    // function(err, hash) - passing anonymous function
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      // if error return a callback
+      if (err) return next(err);
+      user.password = hash;
+      // run callback when whole method is done
+      next();
+    });
+  });
+});
+
+//  compare password in the database and the one that user typed in
+
+//create a custom method
+//can create a custom like methods, but we have remenber it every time. Pre is mongoose methid
+
+UserSchema.methods.comparePassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
+//export mongoose/ export all schema so everyfile can use schema
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 //EXPRESS is a library
 var express = require('express');
 
+//require few libraries to log all the request from the user
+var morgan = require('morgan');
+
+//require mongoose library
+var mongoose = require('mongoose');
+
 //app is referring an express object
 var app = express();
 
+//connect mongoose to the database
+mongoose.connect('mongodb://root:abc123@ds151108.mlab.com:51108/ecommerce', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database");
+  }
+});
+
+//add a middleware
+app.use(morgan('dev'))
+
+// route
+app.get('/', function(req, res){
+  var name = "Tanu";
+  res.json("My name is " + name);
+});
+
+app.get('/catname', function (req, res) {
+  res.json('batman');
+})
 //run the server
 //adds validation to the server
 
