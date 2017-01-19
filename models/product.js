1381a1736ae892781fcc137e1e598932836d1aea
Tanu <tanuuu.21@gmail.com>
2017-01-20 10:30:02 +1100

    Updated Spelling

--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
 
-// mongoosastic is library that uses elastic search to replicate the data from momgoDB
+// mongoosastic is library that uses elastic search to replicate the data from mongoDB
 //to elastic search
 //like you can do that later in project
 //Product.find   -- is valid after mongoosastic
